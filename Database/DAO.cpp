568ef3502b5cc2084e596537b1e15fc5bf6f7fac
TolikPylypchuk <pylypchuk.tolik@gmail.com>
2015-10-04 19:01:12 +0300

    Enclosed code in try/catch blocks + minor optimization

--- Database/DAO.cpp
+++ Database/DAO.cpp
@@ -1,58 +1,143 @@
 #include "DAO.h"
 
+using std::exception;
+using std::cerr;
+using std::endl;
+
+using sql::SQLException;
+
 DAO::DAO(string HOST, string USER, string PASSWORD, string DB):
 	driver(get_driver_instance()),
-	connection(std::auto_ptr<sql::Connection>(driver->connect(HOST, USER, PASSWORD)))
+	connection(auto_ptr<sql::Connection>(driver->connect(HOST, USER, PASSWORD)))
 {
-	connection->setSchema(DB);
+	try
+	{
+		connection->setSchema(DB);
+	} catch (SQLException& exp)
+	{
+		cerr << "Connection to " + DB + " failed.\n";
+		cerr << exp.what() << endl;
+	} catch (exception& exp)
+	{
+		cerr << "An exception in DAO constructor occured.\n";
+		cerr << exp.what() << endl;
+	}
 }
+
+auto_ptr<ResultSet> DAO::getResult(string sql) const
+{
+	auto_ptr<sql::Statement> stmt(connection->createStatement());
+	stmt->execute(sql);
+
+	auto_ptr<ResultSet> res;
+	res.reset(stmt->getResultSet());
+
+	return res;
+}
+
 User DAO::getUserById(unsigned int id) const
 {
-	std::auto_ptr<sql::Statement> stmt(connection->createStatement());
-	stmt->execute("select (id, name, surname, role) from users where id='" + std::to_string(id) + "'");
-	std::auto_ptr< sql::ResultSet > res;
-	res.reset(stmt->getResultSet());	
-	
-	return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	try
+	{
+		auto_ptr<ResultSet> res = getResult("select (id, name, surname, role) from users where id='" + std::to_string(id) + "'");
+		return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	} catch (SQLException& exp)
+	{
+		cerr << "An SQL exception in getUserById() occured.\n";
+		cerr << exp.what() << endl;
+	} catch (exception& exp)
+	{
+		cerr << "An exception in getUserById() occured.\n";
+		cerr << exp.what() << endl;
+	}
+
+	return User();
 }
+
 User DAO::getUserByName(string name) const
 {
-	std::auto_ptr<sql::Statement> stmt(connection->createStatement());
-	stmt->execute("select (id, name, surname, role) from users where name='" + name + "'");
-	std::auto_ptr< sql::ResultSet > res;
-	res.reset(stmt->getResultSet());
+	try
+	{
+		auto_ptr<ResultSet> res = getResult("select (id, name, surname, role) from users where name='" + name + "'");
+		return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	} catch (SQLException& exp)
+	{
+		cerr << "An SQL exception in getUserByName() occured.\n";
+		cerr << exp.what() << endl;
+	} catch (exception& exp)
+	{
+		cerr << "An exception in getUserByName() occured.\n";
+		cerr << exp.what() << endl;
+	}
 
-	return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	return User();
 }
+
 User DAO::getUserBySurname(string surname) const
 {
-	std::auto_ptr<sql::Statement> stmt(connection->createStatement());
-	stmt->execute("select (id, name, surname, role) from users where surname='" + surname + "'");
-	std::auto_ptr< sql::ResultSet > res;
-	res.reset(stmt->getResultSet());
+	try
+	{
+		auto_ptr<ResultSet> res = getResult("select (id, name, surname, role) from users where surname='" + surname + "'");
+		return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	} catch (SQLException& exp)
+	{
+		cerr << "An SQL exception in getUserBySurname() occured.\n";
+		cerr << exp.what() << endl;
+	} catch (exception& exp)
+	{
+		cerr << "An exception in getUserBySurname() occured.\n";
+		cerr << exp.what() << endl;
+	}
 
-	return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	return User();
 }
-User DAO::getUser(string name, string surname) const
+
+User DAO::getUserByFullName(string name, string surname) const
 {
-	std::auto_ptr<sql::Statement> stmt(connection->createStatement());
-	stmt->execute("select (id, name, surname, role) from users where name='" + name + "' AND surname='" + surname + "'");
-	std::auto_ptr< sql::ResultSet > res;
-	res.reset(stmt->getResultSet());
+	try
+	{
+		auto_ptr<ResultSet> res = getResult("select (id, name, surname, role) from users where name='" + name + "' AND surname='" + surname + "'");
+		return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	} catch (SQLException& exp)
+	{
+		cerr << "An SQL exception in getUserByFullName() occured.\n";
+		cerr << exp.what() << endl;
+	} catch (exception& exp)
+	{
+		cerr << "An exception in getUserByFullName() occured.\n";
+		cerr << exp.what() << endl;
+	}
 
-	return User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role"));
+	return User();
 }
+
 vector<User> DAO::getUsersByGroup(string group) const
 {
 	vector<User> result;
-	std::auto_ptr<sql::Statement> stmt(connection->createStatement());
-	stmt->execute("select (id, name, surname, role) from users where group='"+group+"' ORDER BY name");
-	std::auto_ptr< sql::ResultSet > res;
-	do {
-		res.reset(stmt->getResultSet());
-		while (res->next()) {
-			result.push_back(User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role")));
-		}
-	} while (stmt->getMoreResults());
+	try
+	{
+		auto_ptr<sql::Statement> stmt(connection->createStatement());
+		stmt->execute("select (id, name, surname, role) from users where group='" + group + "' ORDER BY name");
+		auto_ptr<ResultSet> res;
+
+		do
+		{
+			res.reset(stmt->getResultSet());
+
+			while (res->next())
+			{
+				result.push_back(User(res->getInt("id"), res->getString("name"), res->getString("surname"), res->getInt("role")));
+			}
+		} while (stmt->getMoreResults());
+	} catch (SQLException& exp)
+	{
+		cerr << "An SQL exception in getUsersByGroup() occured.\n";
+		cerr << exp.what() << endl;
+	} catch (exception& exp)
+	{
+		cerr << "An exception in getUsersByGroup() occured.\n";
+		cerr << exp.what() << endl;
+	}
+
 	return result;
 }
--- Database/DAO.h
+++ Database/DAO.h
@@ -1,9 +1,10 @@
 #pragma once
+
 #include <string>
 #include <vector>
-
 #include <sstream>
 #include <stdexcept>
+
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
@@ -15,24 +16,27 @@
 
 using std::string;
 using std::vector;
+using std::auto_ptr;
+
+using sql::ResultSet;
+
 class DAO
 {
-public:
-	DAO(string HOST, string USER, string PASSWORD, string DB);
-	User getUserById(unsigned int id) const;
-	User getUserByName(string name) const;
-	User getUserBySurname(string surname) const;
-	User getUser(string name, string surname) const;
-	vector<User> getUsersByGroup(string group) const;
-
-protected:
-
-
-private:
 	string HOST;
 	string USER;
 	string PASSWORD;
 	string DB;
 	sql::Driver* driver;
-	std::auto_ptr<sql::Connection> connection;
+	auto_ptr<sql::Connection> connection;
+
+public:
+	DAO(string HOST, string USER, string PASSWORD, string DB);
+	User getUserById(unsigned int id) const;
+	User getUserByName(string name) const;
+	User getUserBySurname(string surname) const;
+	User getUserByFullName(string name, string surname) const;
+	vector<User> getUsersByGroup(string group) const;
+
+private:
+	auto_ptr<ResultSet> getResult(string sql) const;
 };
