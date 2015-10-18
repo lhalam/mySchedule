fd5eef9581b4ee878f882571e57b12f38a13104e
QwerMike <tmikent@gmail.com>
2015-10-18 21:24:28 +0300

    Update: BaseConnection -> MySQLAccess

--- Database/DAOGroup.h
+++ Database/DAOGroup.h
@@ -8,11 +8,11 @@ class DAOGroup : public DAO
 public:
 	//Getters
 
-	Entity * getById(BaseConnection connection, unsigned id) const;
-	Group getByName(BaseConnection connection, string name) const;
+	Entity * getById(MySQLAccess connection, unsigned id) const;
+	Group getByName(MySQLAccess connection, string name) const;
 
 	// Setters
 
-	void addGroup(BaseConnection connection, const Group& group) const;
-	void updateName(BaseConnection connection, const Group& group, string newName) const;
+	void addGroup(MySQLAccess connection, const Group& group) const;
+	void updateName(MySQLAccess connection, const Group& group, string newName) const;
 };
