a7c097bbaf8536346449b2b46e0f77aeec77e6a2
Slovechik <yarikmelnyk39@gmail.com>
2015-10-27 21:11:59 +0200

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -10,6 +10,14 @@ Template::Template(): templ("none")
 
 }
 
+string Template::load(char const * filepath)
+{
+    std::ifstream src(filepath);
+    std::ostringstream buf;
+    buf << src.rdbuf();
+    return buf.str();
+}
+
 string Template::render(map<TKey key, Entity obj> mp)
 {
 	std::string str = std::to_string(mp.find(key));
