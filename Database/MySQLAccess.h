8f06abae7e686ccea061b35c454e181486c6e5d1
QwerMike <tmikent@gmail.com>
2015-11-01 14:20:31 +0200

    Update MySQLAccess.h

--- Database/MySQLAccess.h
+++ Database/MySQLAccess.h
@@ -19,25 +19,16 @@ using namespace std;
 class MySQLAccess {
 public:
 	MySQLAccess(const string, const string, const string, const string);
-	//MySQLAccess(const MySQLAccess&);
-	//MySQLAccess& operator=(const MySQLAccess&);
 	~MySQLAccess();
 	void close();
 	bool execute(const sql::SQLString&);
 	int executeUpdate(const sql::SQLString&);
 	sql::ResultSet* executeQuery(const sql::SQLString&);
-	
-	/* possible realization:
-	/* MySQLAccess - abstract class
-	/* u can only operate with pointers on objects of this class
-	/*
-	//static MySQLAccess* getInstance(const string, const string, 
-	//			const string, const string);
-	 */  
 
 private:
 	void exceptionResult(sql::SQLException&);
-
+	MySQLAccess(const MySQLAccess&) = delete;
+	
 	auto_ptr<sql::Connection> con;
 	auto_ptr<sql::Statement> stmt;
 };
