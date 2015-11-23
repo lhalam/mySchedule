822b55abd76ee77f575bf88dc75e258330b57437
NataliaSlobodianiuk <slo0nata@gmail.com>
2015-11-23 21:14:29 +0200

    merge conflict solved

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include "Server\webserver.h"
 #include "Server\socket.h"
 #include "Server\Request_Handler.h"
-//#include "Config.h"
+#include "Config\Config.h"
 #include <map>
 
 int main()
 {
-	//Config conf("config.txt");
-	//map<string, map<string, string> > res = conf.getConfig();
+	Config conf("config.txt");
+	map<string, map<string, string> > res = conf.getConfig();
 	webserver(8080, Request_Handler);
 
 	return 0;
